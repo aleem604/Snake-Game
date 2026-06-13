5acf3d4b298245dc433e0817c2c6509e9b71bae6
Aleem <aleem604@gmail.com>
2026-06-13 17:15:35 +0500

    segments setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -6,10 +7,36 @@ screen.bgcolor('black')
 screen.title("My Snake Game")
 
 starting_positions = [(0,0), (-20, 0), (-40, 0)]
+segements = []
 
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color('white')
+    new_segment.penup()
     new_segment.goto(position)
+    segements.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(start = 2, stop=0, step=-1):
+        pring(seg_num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 screen.exitonclick()
